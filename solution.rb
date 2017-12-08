a340c43ac756f0e29bab3e5ade1ab6f4798c8207
Fermedina <fermed28@gmai.com>
2017-12-08 10:13:07 -0500

    add string responses

--- solution.rb
+++ solution.rb
@@ -3,8 +3,9 @@ numero=0
 
 get "/" do 
 	if  request.env["HTTP_PERMISO"]	== "soy-un-token-secreto"
-		"<h1/>Bienvenido a la zona secreta</h1>"
+		"Si lo logramos!"
 	else
+		"Sin Permiso"
 	end	
 end
 
